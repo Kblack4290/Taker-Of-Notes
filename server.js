2415e077f576d9c764098625ca830d91bc6f906c
KB Desktop <Keithblack4290gmail.com>
2021-03-21 13:10:34 -0600

    A little pseudo action. Thank you hot restaurant for your inspiration

---
+++ server.js
@@ -0,0 +1,24 @@
+// Hot restaurant is a helpful activity
+
+// Download fs, express, path, 
+
+// establish a port for 8080 process.env.PORT || 8080
+
+
+// use express to handle data parsing
+
+
+// You will need use your routes to connect each html ex "require('../../api')" "require('../../html')"
+//keeping routes in same file will be easier 
+//GET how to get api data from notes.html - app.get(.../api.route/api.route), (req, res) => res.json(data))
+// POST saving notes app.post(api/api.route, req, res) => { if statements}
+// PUT When new notes are entered they will replace the current note
+//Delete Deleting any saved notes.
+
+//HTML ROUTE - Path = app.Get('../htmlpath', (req,res) => { res.sendfile(path.join(__dirname, '../htmlpath'));});
+//Default route app.get('*', (req,res) => { res.sendfile(path.join(__dirname, '../htmlpath'));});
+
+
+
+//add the listener to to make sure you are connected to the port
+
