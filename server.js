9a4d8906a907e69a4eb43b6a5b15b0643236114f
KB Desktop <Keithblack4290gmail.com>
2021-03-21 13:29:00 -0600

    added to gitignore, got start page to pop up on 8080. need to GET /notes

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 // Hot restaurant is a helpful activity
 
 // Download fs, express, path, 
-
+const fs = require('fs');
+const express = require('express');
+const path = require('path')
+const app = express();
 // establish a port for 8080 process.env.PORT || 8080
-
+const PORT = process.env.PORT || 8080;
 
 // use express to handle data parsing
-
+app.use(express.json());
+app.use(express.urlencoded({ extend: true}));
+app.use(express.static('Develop/public'));
 
 // You will need use your routes to connect each html ex "require('../../api')" "require('../../html')"
 //keeping routes in same file will be easier 
@@ -22,3 +27,6 @@
 
 //add the listener to to make sure you are connected to the port
 
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+})
